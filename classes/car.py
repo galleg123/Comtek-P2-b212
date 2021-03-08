2835d1b4d6d90fd5b64fbd27a34dc1d7412dc0b7
galleg123 <mikkelkp360@gmail.com>
2021-03-08 09:47:14 +0100

    Changes

---
+++ classes/car.py
@@ -0,0 +1,6 @@
+import pygame
+import random
+import sys
+
+class car:
+    carImg = pygame.image.load("assets\\car.png")
---
+++ main.py
@@ -0,0 +1,34 @@
+import pygame
+import random
+import sys
+from classes import car
+from pygame import (
+    image,
+    display,
+    init,
+    event,
+    QUIT,
+)
+
+import classes
+
+
+init()
+size = width, height = 1920, 1000
+screen = display.set_mode(size)
+
+
+def main():
+    Car = car.car
+    run = True
+    while run and display.get_active():
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                run = False
+
+        screen.blit(Car.carImg,(100,100))
+
+
+if __name__ == "__main__":
+    main()
+
