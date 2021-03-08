4024e9a94ccacb64e94cf81dc293702c1f73ba83
Karachnor <70699974+Karachnor@users.noreply.github.com>
2021-03-08 08:41:57 +0100

    udgangspunkt pygame

---
+++ Simulation/main.py
@@ -0,0 +1,22 @@
+import pygame
+import random
+import sys
+from pygame import (
+    image,
+    display,
+
+    init,
+)
+
+init()
+size = width, height = 1920, 1000
+screen = display.set_mode(size)
+
+def main():
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type ==pygame.QUIT:
+                run =False
+if __name__ == "__main__":
+   main()
