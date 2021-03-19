92a97923c3272f112b58e2ddd5a4931e21d03d77
galleg123 <mikkelkp360@gmail.com>
2021-03-19 08:41:45 +0100

    Comments

--- classes/road.py
+++ classes/road.py
@@ -11,9 +11,11 @@ from classes import car
 
 
 class road:
-    img = image.load("assets\\road.png")
-    rect = img.get_rect()
+    img = image.load("assets\\road.png")                    #Load image of road
+    rect = img.get_rect()                                   #Define rect as the size of road image
+
+
 
     def __init__(self):
-        self.img = transform.scale(self.img, [1920, 180])
+        self.img = transform.scale(self.img, [1920, 180])   #Change size of road
         self.rect = self.img.get_rect()
