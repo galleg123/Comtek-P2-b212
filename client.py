de3ab2e481209bc904c3e3bf4d22985c3b041c55
Mast3rwaf1z <thomasmollerjensen@live.dk>
2021-03-24 12:44:14 +0100

    fix changes

--- client.py
+++ client.py
@@ -7,7 +7,6 @@ from classes.road import road
 from pygame import TEXTINPUT, image, display, init, event, QUIT, transform
 
 #Global variables
-<<<<<<< HEAD
 data = "placeholder"                                                        #variable with a placeholder value to be uploaded to the server
 locations = {}                                                              #Dictionary which is accessed for the purposes of synchronizing all clients
 clientNum = 0                                                               #The specific number each client has
@@ -57,53 +56,6 @@ class client(threading.Thread):                                             #cla
                 self.started = True                                         #set local variable for determining simulation state to true
             if data.__len__() > 0:                                          #if there is data to send to the host server, send it
                 self.s.send(bytes(data, 'utf-8'))                           #send the data to the host server
-=======
-data = "placeholder"    #variable with a placeholder value to be uploaded to the server
-locations = {}          #Dictionary which is accessed for the purposes of synchronizing all clients
-clientNum = 0           #The specific number each client has
-
-
-# seperate thread to keep the connection to the server going while the simulation is running
-class client(threading.Thread):                 #class definition, inherits from the threading class
-    def __init__(self):                         #initialisation method which is run when class is initialized
-        threading.Thread.__init__(self)         #when the object itself is initialized, initialize the thread
-        self.SERVER_IP = "62.107.59.124"                    #String containing the IP address of the host server
-        self.SERVER_PORT = 8888                             #Integer containing the port number of the host server
-        self.BUFFER_SIZE = 1024                             #buffer size determining how much data is read at a time
-        self.s = socket(AF_INET, SOCK_STREAM)               #the socket object is created
-        self.s.connect((self.SERVER_IP, self.SERVER_PORT))  #Connects the socket to the server IP on the specific port
-
-#Method that is run when the thread is started
-    def run(self):              #method definition
-        global data             #Import the global variable data
-        global locations        #Import locations
-        global clientNum        #Import clientNum
-        self.joined = False     #create local variable to determine if the client has joined the session
-        while True:                                         #run an infinite loop until broken out of or returned
-            In = input("write join to join a session: ")    #wait until user writes a string into the terminal
-            if In.__len__() > 0:                #check if the received string isn't empty
-                self.s.send(bytes(In, 'utf-8')) #send the string to the host server
-                print("data sent.") #debugx
-            if In == "join":        #if the sent string is join, do the following
-                self.joined = True  #set local boolean to true
-                break               #break out of the infinite loop and continue to the simulation
-            if In == "quit":        #if the sent string is quit do the following
-                self.s.close()      #close socket
-                return              #return out of the socket, this is to avoid exceptions when closing the window
-
-        self.started = False                                    #local variable used to determine whether the host has started the simulation or not
-        while self.joined:                                      #run while loop while the client is connected
-            r = self.s.recv(self.BUFFER_SIZE).decode('utf-8')   #receive an encoded string from the host server and decode it
-            if not r == "placeholderplaceholder" and not r.split(",")[0] == "start":    #check if it's a placeholder or another keyword
-                dataArray = r.split(";")                                #decode the string into an array of dictionary formatted variables
-                for d in dataArray:                                     #run for loop for every entry in the array
-                    locations[int(d.split(":")[0])] = d.split(":")[1]   #seperate each array entry into key and value pairs and create a dictionary
-            if r.split(",")[0] == "start":                              #if the received data is start followed by a seperator
-                clientNum = r.split(",")[1]         #set client number to the number after the seperator
-                self.started = True                 #set local variable for determining simulation state to true
-            if data.__len__() > 0:                  #if there is data to send to the host server, send it
-                self.s.send(bytes(data, 'utf-8'))   #send the data to the host server
->>>>>>> 9b94a9a8fc08743d00250b7c752f2618e937aeac
 
     #method that is called to close the connection and stop the program, used to avoid exceptions
     def stop(self):                     #method definition
