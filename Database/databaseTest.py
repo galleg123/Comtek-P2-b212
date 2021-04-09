995c60a824bf04c5e4f223d1f41e63130a4bfd16
YungFlye <kflye20@student.aau.dk>
2021-04-09 11:03:56 +0200

    Max værdi kan findes
    Skabelon til hvordan den maksimale værdi kan findes

--- Database/databaseTest.py
+++ Database/databaseTest.py
@@ -3,10 +3,25 @@ import mysql.connector
 mydb = mysql.connector.connect(
   host="localhost",
   user="testUser",
-  password="b212"
+  password="b212",
+  database="test"
 )
+
+
+test = 0;
+max = 0;
 
 mycursor = mydb.cursor()
 
-mycursor.execute("INSERT INTO test.testtable VALUES (7, 8, 9, 10, 11);")
-mycursor.execute("COMMIT;")
+mycursor.execute("SELECT MAX(test_id) FROM testtable;");
+
+test = mycursor.fetchall()
+for i in test:
+  max = int(i[0])
+
+
+#mycursor.execute("INSERT INTO test.testtable VALUES (7, 8, 9, 10, 11, 4, 2);")
+#mycursor.execute("COMMIT;")
+
+
+print(max);
