ccb37d4865e58f12ce1b5e9d2d550c33e4085e4d
YungFlye <kflye20@student.aau.dk>
2021-05-20 14:21:29 +0200

    test

--- Database/database_Class.py
+++ Database/database_Class.py
@@ -11,26 +11,6 @@ class database(threading.Thread):
         self.test_id = test_id
         self.data = data
 
-    def find_max_value_test_id(self):
-
-        mydb = mysql.connector.connect(
-            host="localhost",
-            user="remote",
-            password="test",
-            database="bil"
-        )
-
-        mycursor = mydb.cursor()
-        try:
-            mycursor.execute("SELECT MAX(test_id) FROM data;")
-
-            test = mycursor.fetchall()
-            for i in test:
-                self.max_test_id = int(i[0])
-        except:
-            self.max_test_id = 0
-        return self.max_test_id+1
-
     def find_max_value_id(self):
         mydb = mysql.connector.connect(
             host="localhost",
@@ -73,9 +53,31 @@ class database(threading.Thread):
         self.upload_to_database(self.test_id, self.data[0], self.data[1], self.data[2])
 def main():
     db = database(1,[1,1,1])
-    db.test_id = db.find_max_value_test_id()
+    db.test_id = find_max_value_test_id()
     db.start()
     print("main executed")
+
+
+def find_max_value_test_id():
+
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="remote",
+        password="test",
+        database="bil"
+    )
+
+    mycursor = mydb.cursor()
+    try:
+        mycursor.execute("SELECT MAX(test_id) FROM data;")
+
+        test = mycursor.fetchall()
+        for i in test:
+            max_test_id = int(i[0])
+    except:
+        max_test_id = 0
+    return max_test_id+1
+
 
 if __name__ == "__main__":
     main()
